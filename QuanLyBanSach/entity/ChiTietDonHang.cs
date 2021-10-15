37e366fe8f1de02ac35a401f1bf7362a616925ef
tkhuyenn <tkhuyen1212@gmail.com>
2021-10-15 10:36:08 +0700

    Tao Dang Nhap va Chi tiet don hang

---
+++ QuanLyBanSach/entity/ChiTietDonHang.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyBanSach.entity
+{
+    class ChiTietDonHang
+    {
+        private int maDonHang;
+        private int maSach;
+        private int soLuong;
+
+        public int getMaDonHang()
+        {
+            return maDonHang;
+        }
+
+        public void setMaDonHang(int maDonHang)
+        {
+            this.maDonHang = maDonHang;
+        }
+
+        public int getMaSach()
+        {
+            return maSach;
+        }
+
+        public void setMaSach(int maSach)
+        {
+            this.maSach = maSach;
+        }
+
+        public int getSoLuong()
+        {
+            return soLuong;
+        }
+
+        public void setSoLuong(int soLuong)
+        {
+            this.soLuong = soLuong;
+        }
+    }
+}
