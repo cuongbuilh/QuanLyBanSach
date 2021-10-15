e00bf946ccf7d44ff660bf8c06722aa6b1a914a9
nhuquynh01 <11194437@st.neu.edu.vn>
2021-10-15 10:40:50 +0700

    tao nha xuat ban va tac gia

---
+++ QuanLyBanSach/entity/NhaXuatBan.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyBanSach.entity
+{
+    class NhaXuatBan
+    {
+        private int maNXB;
+        private string tenNXB;
+        private string diachi;
+
+        public int getMaNXB()
+        {
+            return maNXB;
+        }
+        public void setMaNXB(int maNXB)
+        {
+            this.maNXB = maNXB;
+        }
+
+        public string getTenNXB()
+        {
+            return tenNXB;
+        }
+        public void setTenNXB(string tenNXB)
+        {
+            this.tenNXB = tenNXB;
+        }
+
+        public string getDiachi()
+        {
+            return diachi;
+        }
+        public void setDiachi(string diachi)
+        {
+            this.diachi = diachi;
+        }
+
+    }
+}
---
+++ QuanLyBanSach/entity/TacGia.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyBanSach.entity
+{
+    class TacGia
+    {
+        private int maTacGia;
+        private string tenTacGia;
+        private string mota;
+
+        public int getMaTacGia()
+        {
+            return maTacGia;
+        }
+
+        public void setMaTacGia(int maTacGia)
+        {
+            this.maTacGia = maTacGia;
+        }
+
+        public string getTenNXB()
+        {
+            return tenTacGia;
+        }
+        public void setTenNXB(string tenTacGia)
+        {
+            this.tenTacGia = tenTacGia;
+        }
+
+        public string getMota()
+        {
+            return mota;
+        }
+        public void setMota(string mota)
+        {
+            this.mota = mota;
+        }
+    }
+}
