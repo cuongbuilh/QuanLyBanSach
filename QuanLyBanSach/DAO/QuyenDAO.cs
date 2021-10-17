74dfe11b5b18b8ac1fc0756f675384f4ef8e2201
nhuquynh01 <11194437@st.neu.edu.vn>
2021-10-17 10:40:13 +0700

    sachDAO va QuyenDAO

--- QuanLyBanSach/DAO/QuyenDAO.cs
+++ QuanLyBanSach/DAO/QuyenDAO.cs
@@ -1,12 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using QuanLyBanSach.entity;
+
 
 namespace QuanLyBanSach.DAO
 {
     class QuyenDAO : AbstractDAO
     {
+        public DataTable GetDataTable()
+        {
+            return Lay_DulieuBang("select * from Quyen");
+        }
+
+        public void Insert(Quyen quyen)
+        {
+            string prepare = "insert into TacGia values={{0},'{1}'}";
+            string sql = String.Format(prepare, quyen.getMaQuyen(), quyen.getTenQuyen());
+            ThucThi(sql);
+        }
+        public void Delete(int maQuyen)
+        {
+            // todo-list
+            string prepare = "delete from Quyen where MaQuyen = {0}";
+            string sql = String.Format(prepare, maQuyen);
+            ThucThi(sql);
+        }
+        public void Update(Quyen quyen)
+        {
+            // todo-list
+            string prepare = "update Quyen set TenQuyen = '{0}' where MaTacGia = {1}";
+            string sql = String.Format(prepare, quyen.getTenQuyen(), quyen.getMaQuyen());
+            ThucThi(sql);
+        }
+        public bool IsIDExists(int id)
+        {
+            // todo-list
+            string prepare = "select * from Quyen where MaQuyen = {0}";
+            string sql = String.Format(prepare, id);
+            return DocDuLieu(sql).Read();
+        }
     }
 }
--- QuanLyBanSach/DAO/SachDAO.cs
+++ QuanLyBanSach/DAO/SachDAO.cs
@@ -1,12 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using QuanLyBanSach.entity;
 
 namespace QuanLyBanSach.DAO
 {
     class SachDAO : AbstractDAO
     {
+        public DataTable GetDatatable()
+        {
+            // todo-list
+            return Lay_DulieuBang("select * from Sach");
+        }
+        public void Insert(Sach sach)
+        {
+            // todo-list
+            string prepare = "insert into Sach values={{0},'{1}',{2},{3},{4},{5},'{6}',{7}";
+            string sql = String.Format(prepare, sach.getMaSach(), sach.getTenSach(), sach.getMaNXB(), sach.getMaTacGia(),sach.getGiaBia(),sach.getGiaBan(),sach.getNamXuatBan(), sach.getMaLoai());
+            ThucThi(sql);
+        }
+        public void Delete(int maSach)
+        {
+            // todo-list
+            string prepare = "delete from Sach where MaSach = {0}";
+            string sql = String.Format(prepare, maSach);
+            ThucThi(sql);
+        }
+        public void Update(Sach sach)
+        {
+            // todo-list
+            string prepare = "update Sach set TenSach = '{0}', MaNXB = {1}, MaTacGia = {2}, GiaBia = {3}, GiaBan = {4}, NamXuatBan = '{5}', MaLoai = {6} where MaSach = {7}";
+            string sql = String.Format(prepare, sach.getTenSach(), sach.getMaNXB(), sach.getMaTacGia(), sach.getGiaBia(), sach.getGiaBan(), sach.getNamXuatBan(), sach.getMaLoai(), sach.getMaSach());
+            ThucThi(sql);
+        }
+        public bool IsIDExists(int id)
+        {
+            // todo-list
+            string prepare = "select * from Sach where id = {0}";
+            string sql = String.Format(prepare, id);
+            return DocDuLieu(sql).Read();
+        }
     }
 }
