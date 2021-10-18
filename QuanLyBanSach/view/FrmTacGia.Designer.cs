bd12606a5e5a5e3c54fbc3397a15b59337c7b04c
Phạm Quang Huy <phamquanghuyk61@gmail.com>
2021-10-18 09:54:09 +0700

    Tao form TacGia & TheLoai

--- QuanLyBanSach/view/FrmTacGia.Designer.cs
+++ QuanLyBanSach/view/FrmTacGia.Designer.cs
@@ -29,12 +29,184 @@ namespace QuanLyBanSach.view
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.lbl_TacGia = new System.Windows.Forms.Label();
+            this.lbl_MaTacGia = new System.Windows.Forms.Label();
+            this.lbl_TenTacGia = new System.Windows.Forms.Label();
+            this.lbl_MoTa = new System.Windows.Forms.Label();
+            this.view_TacGia = new System.Windows.Forms.DataGridView();
+            this.txt_MaTacGia = new System.Windows.Forms.TextBox();
+            this.txt_TenTacGia = new System.Windows.Forms.TextBox();
+            this.txt_MoTa = new System.Windows.Forms.TextBox();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.btn_Sua = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btn_Thoat = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.view_TacGia)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnThem
+            // 
+            this.btnThem.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThem.Location = new System.Drawing.Point(636, 49);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(118, 52);
+            this.btnThem.TabIndex = 0;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            // 
+            // lbl_TacGia
+            // 
+            this.lbl_TacGia.AutoSize = true;
+            this.lbl_TacGia.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_TacGia.Location = new System.Drawing.Point(339, 9);
+            this.lbl_TacGia.Name = "lbl_TacGia";
+            this.lbl_TacGia.Size = new System.Drawing.Size(95, 25);
+            this.lbl_TacGia.TabIndex = 1;
+            this.lbl_TacGia.Text = "TÁC GIẢ";
+            // 
+            // lbl_MaTacGia
+            // 
+            this.lbl_MaTacGia.AutoSize = true;
+            this.lbl_MaTacGia.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_MaTacGia.Location = new System.Drawing.Point(60, 57);
+            this.lbl_MaTacGia.Name = "lbl_MaTacGia";
+            this.lbl_MaTacGia.Size = new System.Drawing.Size(118, 25);
+            this.lbl_MaTacGia.TabIndex = 1;
+            this.lbl_MaTacGia.Text = "Mã tác giả:";
+            // 
+            // lbl_TenTacGia
+            // 
+            this.lbl_TenTacGia.AutoSize = true;
+            this.lbl_TenTacGia.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_TenTacGia.Location = new System.Drawing.Point(60, 112);
+            this.lbl_TenTacGia.Name = "lbl_TenTacGia";
+            this.lbl_TenTacGia.Size = new System.Drawing.Size(125, 25);
+            this.lbl_TenTacGia.TabIndex = 1;
+            this.lbl_TenTacGia.Text = "Tên tác giả:";
+            // 
+            // lbl_MoTa
+            // 
+            this.lbl_MoTa.AutoSize = true;
+            this.lbl_MoTa.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_MoTa.Location = new System.Drawing.Point(60, 171);
+            this.lbl_MoTa.Name = "lbl_MoTa";
+            this.lbl_MoTa.Size = new System.Drawing.Size(72, 25);
+            this.lbl_MoTa.TabIndex = 1;
+            this.lbl_MoTa.Text = "Mô tả:";
+            // 
+            // view_TacGia
+            // 
+            this.view_TacGia.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.view_TacGia.Location = new System.Drawing.Point(78, 212);
+            this.view_TacGia.Name = "view_TacGia";
+            this.view_TacGia.Size = new System.Drawing.Size(508, 208);
+            this.view_TacGia.TabIndex = 2;
+            // 
+            // txt_MaTacGia
+            // 
+            this.txt_MaTacGia.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_MaTacGia.Location = new System.Drawing.Point(212, 57);
+            this.txt_MaTacGia.Name = "txt_MaTacGia";
+            this.txt_MaTacGia.Size = new System.Drawing.Size(374, 31);
+            this.txt_MaTacGia.TabIndex = 3;
+            // 
+            // txt_TenTacGia
+            // 
+            this.txt_TenTacGia.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_TenTacGia.Location = new System.Drawing.Point(212, 109);
+            this.txt_TenTacGia.Name = "txt_TenTacGia";
+            this.txt_TenTacGia.Size = new System.Drawing.Size(374, 31);
+            this.txt_TenTacGia.TabIndex = 3;
+            // 
+            // txt_MoTa
+            // 
+            this.txt_MoTa.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_MoTa.Location = new System.Drawing.Point(212, 165);
+            this.txt_MoTa.Name = "txt_MoTa";
+            this.txt_MoTa.Size = new System.Drawing.Size(374, 31);
+            this.txt_MoTa.TabIndex = 3;
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLuu.Location = new System.Drawing.Point(636, 127);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(118, 52);
+            this.btnLuu.TabIndex = 0;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.UseVisualStyleBackColor = true;
+            // 
+            // btn_Sua
+            // 
+            this.btn_Sua.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Sua.Location = new System.Drawing.Point(636, 212);
+            this.btn_Sua.Name = "btn_Sua";
+            this.btn_Sua.Size = new System.Drawing.Size(118, 52);
+            this.btn_Sua.TabIndex = 0;
+            this.btn_Sua.Text = "Sửa";
+            this.btn_Sua.UseVisualStyleBackColor = true;
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXoa.Location = new System.Drawing.Point(636, 290);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(118, 52);
+            this.btnXoa.TabIndex = 0;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            // 
+            // btn_Thoat
+            // 
+            this.btn_Thoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Thoat.Location = new System.Drawing.Point(636, 368);
+            this.btn_Thoat.Name = "btn_Thoat";
+            this.btn_Thoat.Size = new System.Drawing.Size(118, 52);
+            this.btn_Thoat.TabIndex = 0;
+            this.btn_Thoat.Text = "Thoát";
+            this.btn_Thoat.UseVisualStyleBackColor = true;
+            // 
+            // FrmTacGia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_MoTa);
+            this.Controls.Add(this.txt_TenTacGia);
+            this.Controls.Add(this.txt_MaTacGia);
+            this.Controls.Add(this.view_TacGia);
+            this.Controls.Add(this.lbl_MoTa);
+            this.Controls.Add(this.lbl_TenTacGia);
+            this.Controls.Add(this.lbl_MaTacGia);
+            this.Controls.Add(this.lbl_TacGia);
+            this.Controls.Add(this.btn_Thoat);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btn_Sua);
+            this.Controls.Add(this.btnLuu);
+            this.Controls.Add(this.btnThem);
+            this.Name = "FrmTacGia";
             this.Text = "FrmTacGia";
+            ((System.ComponentModel.ISupportInitialize)(this.view_TacGia)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Label lbl_TacGia;
+        private System.Windows.Forms.Label lbl_MaTacGia;
+        private System.Windows.Forms.Label lbl_TenTacGia;
+        private System.Windows.Forms.Label lbl_MoTa;
+        private System.Windows.Forms.DataGridView view_TacGia;
+        private System.Windows.Forms.TextBox txt_MaTacGia;
+        private System.Windows.Forms.TextBox txt_TenTacGia;
+        private System.Windows.Forms.TextBox txt_MoTa;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.Button btn_Sua;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btn_Thoat;
     }
 }
--- QuanLyBanSach/view/FrmTheLoai.Designer.cs
+++ QuanLyBanSach/view/FrmTheLoai.Designer.cs
@@ -29,12 +29,160 @@ namespace QuanLyBanSach.view
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbl_ThongBao = new System.Windows.Forms.Label();
+            this.lbl_MaLoai = new System.Windows.Forms.Label();
+            this.lbl_TenLoai = new System.Windows.Forms.Label();
+            this.txt_MaLoai = new System.Windows.Forms.TextBox();
+            this.txt_TenLoai = new System.Windows.Forms.TextBox();
+            this.view_TheLoai = new System.Windows.Forms.DataGridView();
+            this.btn_Them = new System.Windows.Forms.Button();
+            this.btn_Luu = new System.Windows.Forms.Button();
+            this.btn_Sua = new System.Windows.Forms.Button();
+            this.btn_Xoa = new System.Windows.Forms.Button();
+            this.btn_Thoat = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.view_TheLoai)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_ThongBao
+            // 
+            this.lbl_ThongBao.AutoSize = true;
+            this.lbl_ThongBao.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_ThongBao.Location = new System.Drawing.Point(333, 31);
+            this.lbl_ThongBao.Name = "lbl_ThongBao";
+            this.lbl_ThongBao.Size = new System.Drawing.Size(107, 25);
+            this.lbl_ThongBao.TabIndex = 0;
+            this.lbl_ThongBao.Text = "THỂ LOẠI";
+            // 
+            // lbl_MaLoai
+            // 
+            this.lbl_MaLoai.AutoSize = true;
+            this.lbl_MaLoai.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_MaLoai.Location = new System.Drawing.Point(81, 88);
+            this.lbl_MaLoai.Name = "lbl_MaLoai";
+            this.lbl_MaLoai.Size = new System.Drawing.Size(88, 25);
+            this.lbl_MaLoai.TabIndex = 0;
+            this.lbl_MaLoai.Text = "Mã loại:";
+            // 
+            // lbl_TenLoai
+            // 
+            this.lbl_TenLoai.AutoSize = true;
+            this.lbl_TenLoai.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_TenLoai.Location = new System.Drawing.Point(79, 168);
+            this.lbl_TenLoai.Name = "lbl_TenLoai";
+            this.lbl_TenLoai.Size = new System.Drawing.Size(95, 25);
+            this.lbl_TenLoai.TabIndex = 0;
+            this.lbl_TenLoai.Text = "Tên loại:";
+            // 
+            // txt_MaLoai
+            // 
+            this.txt_MaLoai.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_MaLoai.Location = new System.Drawing.Point(213, 88);
+            this.txt_MaLoai.Name = "txt_MaLoai";
+            this.txt_MaLoai.Size = new System.Drawing.Size(305, 31);
+            this.txt_MaLoai.TabIndex = 1;
+            // 
+            // txt_TenLoai
+            // 
+            this.txt_TenLoai.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_TenLoai.Location = new System.Drawing.Point(213, 162);
+            this.txt_TenLoai.Name = "txt_TenLoai";
+            this.txt_TenLoai.Size = new System.Drawing.Size(305, 31);
+            this.txt_TenLoai.TabIndex = 1;
+            // 
+            // view_TheLoai
+            // 
+            this.view_TheLoai.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.view_TheLoai.Location = new System.Drawing.Point(84, 235);
+            this.view_TheLoai.Name = "view_TheLoai";
+            this.view_TheLoai.Size = new System.Drawing.Size(434, 180);
+            this.view_TheLoai.TabIndex = 2;
+            // 
+            // btn_Them
+            // 
+            this.btn_Them.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Them.Location = new System.Drawing.Point(607, 76);
+            this.btn_Them.Name = "btn_Them";
+            this.btn_Them.Size = new System.Drawing.Size(121, 48);
+            this.btn_Them.TabIndex = 3;
+            this.btn_Them.Text = "Thêm";
+            this.btn_Them.UseVisualStyleBackColor = true;
+            // 
+            // btn_Luu
+            // 
+            this.btn_Luu.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Luu.Location = new System.Drawing.Point(607, 153);
+            this.btn_Luu.Name = "btn_Luu";
+            this.btn_Luu.Size = new System.Drawing.Size(121, 48);
+            this.btn_Luu.TabIndex = 3;
+            this.btn_Luu.Text = "Lưu";
+            this.btn_Luu.UseVisualStyleBackColor = true;
+            // 
+            // btn_Sua
+            // 
+            this.btn_Sua.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Sua.Location = new System.Drawing.Point(607, 224);
+            this.btn_Sua.Name = "btn_Sua";
+            this.btn_Sua.Size = new System.Drawing.Size(121, 48);
+            this.btn_Sua.TabIndex = 3;
+            this.btn_Sua.Text = "Sửa";
+            this.btn_Sua.UseVisualStyleBackColor = true;
+            // 
+            // btn_Xoa
+            // 
+            this.btn_Xoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Xoa.Location = new System.Drawing.Point(607, 298);
+            this.btn_Xoa.Name = "btn_Xoa";
+            this.btn_Xoa.Size = new System.Drawing.Size(121, 48);
+            this.btn_Xoa.TabIndex = 3;
+            this.btn_Xoa.Text = "Xóa";
+            this.btn_Xoa.UseVisualStyleBackColor = true;
+            // 
+            // btn_Thoat
+            // 
+            this.btn_Thoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Thoat.Location = new System.Drawing.Point(607, 367);
+            this.btn_Thoat.Name = "btn_Thoat";
+            this.btn_Thoat.Size = new System.Drawing.Size(121, 48);
+            this.btn_Thoat.TabIndex = 3;
+            this.btn_Thoat.Text = "Thoát";
+            this.btn_Thoat.UseVisualStyleBackColor = true;
+            // 
+            // FrmTheLoai
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_Thoat);
+            this.Controls.Add(this.btn_Xoa);
+            this.Controls.Add(this.btn_Sua);
+            this.Controls.Add(this.btn_Luu);
+            this.Controls.Add(this.btn_Them);
+            this.Controls.Add(this.view_TheLoai);
+            this.Controls.Add(this.txt_TenLoai);
+            this.Controls.Add(this.txt_MaLoai);
+            this.Controls.Add(this.lbl_TenLoai);
+            this.Controls.Add(this.lbl_MaLoai);
+            this.Controls.Add(this.lbl_ThongBao);
+            this.Name = "FrmTheLoai";
             this.Text = "FrmTheLoai";
+            ((System.ComponentModel.ISupportInitialize)(this.view_TheLoai)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbl_ThongBao;
+        private System.Windows.Forms.Label lbl_MaLoai;
+        private System.Windows.Forms.Label lbl_TenLoai;
+        private System.Windows.Forms.TextBox txt_MaLoai;
+        private System.Windows.Forms.TextBox txt_TenLoai;
+        private System.Windows.Forms.DataGridView view_TheLoai;
+        private System.Windows.Forms.Button btn_Them;
+        private System.Windows.Forms.Button btn_Luu;
+        private System.Windows.Forms.Button btn_Sua;
+        private System.Windows.Forms.Button btn_Xoa;
+        private System.Windows.Forms.Button btn_Thoat;
     }
 }
