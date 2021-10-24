9d87544f9b30922244c41e4a8be645bbb2565b36
Phạm Quang Huy <phamquanghuyk61@gmail.com>
2021-10-24 15:11:44 +0700

    pull and push

--- QuanLyBanSach/view/FrmTheLoai.cs
+++ QuanLyBanSach/view/FrmTheLoai.cs
@@ -19,9 +19,114 @@ namespace QuanLyBanSach.view
         public FrmTheLoai()
         {
             InitializeComponent();
-            adoUtils = new ADOUtils;
+            adoUtils = new ADOUtils();
         }
 
+        private void FrmTheLoai_Load(object sender, EventArgs e)
+        {
+            LoadDataToForm();
+            BindingData();
+        }
 
+        private void LoadDataToForm()
+        {
+            view_TheLoai.DataSource = adoUtils.GetDataTable("select * from THELOAI");
+            
+        }
+
+        private void BindingData()
+        {
+            txt_MaLoai.Clear();
+            txt_MaLoai.DataBindings.Add("Text", view_TheLoai.DataSource, "MALOAI");
+
+            txt_TenLoai.Clear();
+            txt_TenLoai.DataBindings.Add("Text", view_TheLoai.DataSource, "TENLOAI");
+
+        }
+
+        private void btn_Them_Click(object sender, EventArgs e)
+        {
+            string maLoai = txt_MaLoai.Text;
+            string tenLoai = txt_TenLoai.Text;
+           
+
+            string prepapre = "insert into THELOAI values ('{0}','{1}');";
+            string sql = String.Format(prepapre, maLoai, tenLoai);
+            try
+            {
+                adoUtils.Excute(sql);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+            LoadDataToForm();
+        }
+
+        private void btn_Luu_Click(object sender, EventArgs e)
+        {
+            string maLoai = txt_MaLoai.Text;
+            string tenLoai = txt_TenLoai.Text;
+            
+
+            string prepare =
+                "insert into THELOAI values ('{0}', '{1}');";
+            string sql = String.Format(prepare, maLoai, tenLoai);
+
+            try
+            {
+                adoUtils.Excute(sql);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+            LoadDataToForm();
+            btn_Luu.Enabled = false;
+        }
+
+        private void btn_Sua_Click(object sender, EventArgs e)
+        {
+            string maLoai = txt_MaLoai.Text;
+            string tenLoai = txt_TenLoai.Text;
+            
+
+            string prepapre = "update THELOAI set TENLOAI='{0}' where MALOAI='{1}';";
+            string sql = String.Format(prepapre, tenLoai, maLoai);
+
+            try
+            {
+                adoUtils.Excute(sql);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+
+        private void btn_Xoa_Click(object sender, EventArgs e)
+        {
+            string maLoai = txt_MaLoai.Text;
+            string prepare = "delete from THELOAI where MALOAI = '{0}'";
+            string sql = String.Format(prepare, maLoai);
+
+            DialogResult confirmDialogResult = MessageBox.Show("bạn muốn xóa mã loại? " + maLoai);
+            if (confirmDialogResult == DialogResult.OK)
+            {
+                try
+                {
+                    adoUtils.Excute(sql);
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show(exception.Message);
+                }
+            }
+        }
+
+        private void btn_Thoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
